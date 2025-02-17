8ee66f9635af01f83bb008269c91b77b114ec76a
anupriya <anupriyatilouthu@gmail.com>
2025-02-17 15:46:01 +0530

    first-commit

--- backend/Routes/orderRoutes.js
+++ backend/Routes/orderRoutes.js
@@ -4,7 +4,7 @@ const db = require("../db");
 
 // Place an order
 router.post("/", (req, res) => {
-    const { pizza_id, user_name, quantity } = req.body;
+    const {pizza_id, user_name, quantity } = req.body;
     const query = "INSERT INTO order_details (pizza_id, user_name, quantity) VALUES (?, ?, ?)";
 
     db.query(query, [pizza_id, user_name, quantity], (err, results) => {
